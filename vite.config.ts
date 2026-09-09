da99bbf1621e69eb0cb6c180996df23675e79e80
mozzam123 <mozzam607@gmail.com>
2026-09-09 20:25:41 +0530

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ].filter(Boolean),
-  base: "",
+  base: "/MyPortfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
